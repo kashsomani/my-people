0468e2aceb0ff843f4f51f9419f3fe7148028f7f
Kash Somani <kash@kashsomani.com>
2021-07-13 14:33:39 +0530

    modified purgecss to use tailwind in svelte

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 const tailwindcss = require("tailwindcss");
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.svelte', './src/**/*.html'],
+  whitelistPatterns: [/svelte-/],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 
 module.exports = {
   plugins: [tailwindcss("./tailwind.config.js")],
+  ...(!process.env.ROLLUP_WATCH ? [purgecss] : [])
 };
