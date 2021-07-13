15390f36e470c8621890ef42d2b2954c28324fff
Kash Somani <kash@kashsomani.com>
2021-07-13 15:10:38 +0530

    better formatting

--- src/components/Person.svelte
+++ src/components/Person.svelte
@@ -1,14 +1,17 @@
 <script>
   export let person;
 </script>
-<div class="red-border">
+<div class="container red-border grid grid-cols-1">
   {#each person as attr}
-    <p>{`${attr[0]}: ${attr[1]}`}</p>
+    <div class="grid grid-cols-4 gap-0">
+      <div class="col-span-1 p-4"><p>{`${attr[0]}: `}</p></div>
+      <div class="col-span-3 p-4 text-center"><p>{`${attr[1]}`}</p></div>
+    </div>
   {/each}
 </div>
 
 <style>
-  div{
-    @apply grid grid-cols-1;
+  .container{
+    @apply grid grid-cols-1 p-1;
   }
 </style>
