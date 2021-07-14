807494e61264e2143e50869122c0fb11b2f5fc14
Kash Somani <kash@kashsomani.com>
2021-07-14 10:13:37 +0530

    form has started to work to add a new person to database

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,11 +1,35 @@
 <script>
   import {fade} from 'svelte/transition'
+  import {
+    addPerson
+  } from '../firebase'
   const inputs = [
-    "Name",
-    "Age",
-    "Job",
-    "Phone"
-  ]
+    "name",
+    "age",
+    "job",
+    "phone"
+  ];
+  $:error = false
+  $:success = false
+  async function addP(person) {
+    if(Object.keys(person).length !== 0){
+      console.log(`${Object.entries(person).length}`)
+      error = false
+      success = true
+      await addPerson(person)
+    }
+    else {
+      error = true;
+    }
+  }
+  $:person = {}
+  $:clean= (()=>{
+    for(let i in person){
+      if(!person[i]){
+        delete person[i]
+      }
+    }
+  })()
 </script>
 
 <div class="red-border" transition:fade="{{duration:1000}}">
@@ -13,17 +37,31 @@
     {#each inputs as field}
     <div >
       <p class="p-2 text-center">{field}</p>
-      <input type="text" name="{field}" value="">
+      <input type="text" name="{field}" bind:value={person[field]}>
     </div>
     {/each}
   </div>
+  <div transition:fade="{{duration:2000}}" class="grid grid-cols-1 place-items-center">
+    <button on:click={()=>{addPerson(person)}}>
+      ADD
+    </button>
+  </div>
+  <div class="grid grid-cols-1 place-items-center">
+    {#if error}
+      <p>error!!!</p>
+    {/if}
+    {#if success}
+      <p>successsssss!!!</p>
+    {/if}
+  </div>
 </div>
+<p>{`${JSON.stringify(person)}`}</p>
 <style >
   input{
-    @apply bg-yellow-900 text-yellow-300 p-2 border-2 rounded-lg border-transparent;
+    @apply bg-blue-200 text-blue-300 p-2 border-2 rounded-lg border-transparent;
   }
   input:focus{
     outline:none;
-    @apply border-yellow-500;
+    @apply border-blue-900;
   }
 </style>
