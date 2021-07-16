f6b7215f507b85c69cc894b5a17fb99d510478ed
Kash Somani <kash@kashsomani.com>
2021-07-16 14:04:25 +0530

    added enter key support for login

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -14,15 +14,20 @@
         console.log(val)
       }
   }
+  function enter(event){
+    if(event.key==="Enter"){
+      signInWithCheck(email,password)
+    }
+  }
 </script>
 <div class="container">
   <p class="p-2 text-center">EMAIL</p>
   <div class="p-4 bg-blue-300 rounded rounded-full m-2 ">
-    <input type="text" name="email" bind:value={email}>
+    <input type="text" name="email" bind:value={email} on:keydown={enter}>
   </div>
   <p class="p-2 text-center">PASSWORD</p>
   <div class="p-4 bg-blue-300 rounded rounded-full m-2 ">
-    <input type="password" name="password" bind:value={password} >
+    <input type="password" name="password" bind:value={password} on:keydown={enter}>
   </div>
 
   <div class="p-4 bg-blue-300 rounded rounded-full m-2">
